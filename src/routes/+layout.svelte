aed9ab231b5a94ef604e8addae2a959637638103
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-07-27 12:23:22 -0700

    layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Footer from '$lib/components/footer.svelte'
+	import Header from '$lib/components/header.svelte'
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
+<Footer />
+
+<style>
+</style>
